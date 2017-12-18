2b8f8aae5acb5d1182e8c640502cda1dae59bc7c
Nick Teets <nickteets@gmail.com>
2017-12-18 10:58:38 -0700

    Display items for GET endpoint on DOM

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,24 @@
-console.log('hooked up!')
+const fetchItems = () => {
+  fetch('/api/v1/garage_items')
+    .then(response => response.json())
+    .then(items => {
+      items.forEach(item => {
+        appendItem(item);
+      })
+    })
+};
+
+const appendItem = (item) => {
+  const appendableItem  =
+    `<div>
+      <h3>${item.item_name}</h3>
+      <p>${item.reason}</p>
+      <p>${item.cleanliness}</p>
+    </div>`
+
+  $('.garage').append(appendableItem);
+}
+
+$(document).ready(() => {
+  fetchItems();
+});
