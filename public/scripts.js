895f4397d72dcf57b7bc9fef7b34b608be2835aa
Nick Teets <nickteets@gmail.com>
2017-12-18 15:00:56 -0700

    Make scripts linty

--- public/scripts.js
+++ public/scripts.js
@@ -8,19 +8,40 @@ const optionFinder = (selectedOption) => {
   });
 };
 
+const updateCleanliness = (id, selected) => {
+  fetch(`/api/v1/garage_items/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ cleanliness: selected }),
+  })
+    .then((response) => {
+      if (response.status === 204) {
+        return response.status;
+      }
+    })
+    .catch((error) => { throw error; });
+};
+
 const appendItem = (item) => {
   const appendableItem =
-    `<div>
+    `<div class="${item.id}">
       <h3>${item.item_name}</h3>
-      <p>${item.reason}</p>
-      <select class="appended-selector">
-        <option selected value=${item.cleanliness}>${item.cleanliness}</option>
-        <option>${optionFinder(item.cleanliness)[0]}</option>
-        <option>${optionFinder(item.cleanliness)[1]}</option>
-      </select>
+      <div class="item-info">
+        <p>${item.reason}</p>
+        <select class="appended-selector-${item.id}">
+          <option selected value=${item.cleanliness}>${item.cleanliness}</option>
+          <option>${optionFinder(item.cleanliness)[0]}</option>
+          <option>${optionFinder(item.cleanliness)[1]}</option>
+        </select>
+      </div>
     </div>`;
 
   $('.garage').append(appendableItem);
+  $(`.appended-selector-${item.id}`).on('change', () => {
+    updateCleanliness(item.id, $(`.appended-selector-${item.id}`).val());
+  });
 };
 
 const fetchItems = () => {
@@ -33,6 +54,8 @@ const fetchItems = () => {
       });
     });
 };
+
+fetchItems();
 
 const newItem = (event) => {
   event.preventDefault();
@@ -63,12 +86,5 @@ const newItem = (event) => {
     .catch((error) => { throw error; });
 };
 
-// const updateCleanliness = (item) => {
-//   console.log(item);
-// };
-
-$(document).ready(() => {
-  fetchItems();
-});
 
 $('.submit-item').on('click', newItem);
--- server.js
+++ server.js
@@ -14,9 +14,8 @@ const checkParams = require('./utils/checkParams');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(`${__dirname}/public`));
-
 app.set('port', process.env.NODE_ENV || 3000);
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/api/v1/garage_items', (request, response) => {
   database('garage_items').select()
@@ -56,6 +55,11 @@ app.patch('/api/v1/garage_items/:id', (request, response) => {
     .catch((error) => {
       response.status(500).json({ error });
     });
+});
+
+app.listen(app.get('port'), () => {
+// eslint-disable-next-line
+  console.log(`Server is running on ${app.get('port')}.`);
 });
 
 module.exports = app;
